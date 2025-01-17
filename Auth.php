7a010f11ba579b366f3365b015e010a6f0a18c2c
ijunnn <faizunaziz53@gmail.com>
2025-01-18 04:19:50 +0700

    initial commit

---
+++ Auth.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Config;
+
+use Myth\Auth\Config\Auth as MythAuthConfig;
+
+class Auth extends MythAuthConfig
+{
+    /**
+     * --------------------------------------------------------------------
+     * Require Confirmation Registration via Email
+     * --------------------------------------------------------------------
+     *
+     * When enabled, every registered user will receive an email message
+     * with an activation link to confirm the account.
+     *
+     * @var string|null Name of the ActivatorInterface class
+     */
+    public $requireActivation = null;
+    // public $requireActivation = 'Myth\Auth\Authentication\Activators\EmailActivator';
+
+    /**
+     * --------------------------------------------------------------------
+     * Allow Password Reset via Email
+     * --------------------------------------------------------------------
+     *
+     * When enabled, users will have the option to reset their password
+     * via the specified Resetter. Default setting is email.
+     *
+     * @var string|null Name of the ResetterInterface class
+     */
+    public $activeResetter = null;
+    // public $activeResetter = 'Myth\Auth\Authentication\Resetters\EmailResetter';
+
+    /**
+     * --------------------------------------------------------------------
+     * Views used by Auth Controllers
+     * --------------------------------------------------------------------
+     *
+     * @var array
+     */
+    public $views = [
+        'login'           => 'Myth\Auth\Views\login', // connect your login files
+        'register'        => 'Myth\Auth\Views\register', // connect your register files
+        'forgot'          => 'Myth\Auth\Views\forgot',
+        'reset'           => 'Myth\Auth\Views\reset',
+        'emailForgot'     => 'Myth\Auth\Views\emails\forgot',
+        'emailActivation' => 'Myth\Auth\Views\emails\activation',
+    ];
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,38 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => null,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => null,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'codeigniter4/authentication-implementation' => array(
+            'dev_requirement' => false,
+            'provided' => array(
+                0 => '1.0',
+            ),
+        ),
+        'myth/auth' => array(
+            'pretty_version' => 'v1.2.1',
+            'version' => '1.2.1.0',
+            'reference' => '7d0347faa47bd754d2dc49593a9852800be73986',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../myth/auth',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
